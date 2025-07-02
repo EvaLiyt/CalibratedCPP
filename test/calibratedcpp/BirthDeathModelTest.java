b740121e66384ab0d71e32fdd5c63ca8939f2ef6
Marcus Overwater <moverwater@ethz.ch>
2025-07-02 19:37:44 +0200

    Created test directory

---
+++ test/calibratedcpp/BirthDeathModelTest.java
@@ -0,0 +1,16 @@
+package calibratedcpp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BirthDeathModelTest {
+
+    @Test
+    void calculateLogDensity() {
+    }
+
+    @Test
+    void calculateLogCDF() {
+    }
+}
---
+++ test/calibratedcpp/evolution/speciation/CalibratedCoalescentPointProcessTest.java
@@ -0,0 +1,16 @@
+package calibratedcpp.evolution.speciation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalibratedCoalescentPointProcessTest {
+
+    @Test
+    void calculateUnConditionedTreeLogLikelihood() {
+    }
+
+    @Test
+    void calculateLogMarginalDensityOfCalibrations() {
+    }
+}
